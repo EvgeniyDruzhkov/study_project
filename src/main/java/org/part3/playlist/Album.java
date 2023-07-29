dd7d5e2afee8d5df9092129a77dc8195a4e5e9c6
Evgeniy Druzhkov <evgeniydruzhkov38@gmail.com>
2023-07-29 19:09:50 +0300

    Create custom exception and implement addToPlayList methods.

--- src/main/java/org/part3/playlist/Album.java
+++ src/main/java/org/part3/playlist/Album.java
@@ -12,6 +12,7 @@ public class Album {
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
+        songs = new ArrayList<>();
     }
 
     public boolean addSong(String song, double durationOfSong) {
@@ -20,18 +21,28 @@ public class Album {
 
     private Song findSong(String titleOfSong) {
         return songs.stream()
-                .filter(title -> titleOfSong.equals(title.getTitle()))
-                .findFirst()
-                .orElse(null);
+                .filter(song -> titleOfSong.equals(song.getTitle()))
+                .findAny()
+                .orElseThrow(NoSuchSongException::new);
 
     }
 
     public boolean addToPlayList(int numberOfSong, LinkedList<Song> playlist) {
-        return false;
+        if (songs.size() <= numberOfSong)
+            return false;
+        return playlist.add(songs.get(numberOfSong));
     }
 
     public boolean addToPlayList(String titleOfSong, LinkedList<Song> playlist) {
-        return true;
+        Song song;
+        try {
+            song = findSong(titleOfSong);
+        }
+        catch (NoSuchSongException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return playlist.add(song);
     }
 
 }
